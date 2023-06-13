940184c49a72b7cb1e168e6cd71b3e008e0b28f3
Bradley Troll <bradleytroll@gmail.com>
2023-06-13 15:10:11 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const divide = (num1, num2) => num1 / num2;
-const square = (num1) => num1 * num1
-const add = (num1, num2) => num1 + num2;
+//Redid this assignment for practice. New code below; oroginal code commented out.
+
+const divide = (a, b) => a / b;
+const square = a => a ** a;
+const add = (a, b) => a + b;
+
+// const divide = (num1, num2) => num1 / num2;
+// const square = (num1) => num1 * num1
+// const add = (num1, num2) => num1 + num2;
